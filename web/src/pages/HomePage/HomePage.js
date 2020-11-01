1c645294b4115cfb5f3859f4b3556de56984686c
Dave Grace <boadicea45@gmail.com>
2020-11-01 16:21:09 +1100

    011120

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+//import { Link, routes } from '@redwoodjs/router'//
+import NavBarLayout from 'src/layouts/NavBarLayout'
+import CandidateLayout from 'src/layouts/CandidateLayout'
+const HomePage = () => {
+  return (
+    <>
+      <nav>
+        return <NavBarLayout></NavBarLayout>
+      </nav>
+      <main>
+        return <CandidateLayout></CandidateLayout>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
